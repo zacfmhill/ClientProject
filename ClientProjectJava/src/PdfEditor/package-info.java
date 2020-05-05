902c35a7946153e1c7b633185901a234d6991e49
zacfmhill <zacfmhill@gmail.com>
2020-05-04 21:34:11 -0400

    additional file management

---
+++ ClientProjectJava/src/PdfEditor/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author zacfm
+ *
+ */
+package PdfEditor;
---
+++ ClientProjectJava/src/TestApplicationFiles/TestApp.java
@@ -0,0 +1,9 @@
+package TestApplicationFiles;
+public class TestApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
