6ff4ea49dea5ddc97b7e36887478a22ed7c9e5a6
zacfmhill <zacfmhill@gmail.com>
2020-05-10 16:47:59 -0400

    Created XML editing class and imported all files for APACHE poi

--- ClientProjectJava/src/TestApplicationFiles/TestApp.java
+++ ClientProjectJava/src/TestApplicationFiles/TestApp.java
@@ -1,12 +1,16 @@
 package TestApplicationFiles;
 
+import fileEdit.XMLedit;
+import java.io.*;
+import org.apache.poi.xssf.usermodel.*;
 public class TestApp {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static void main(String[] args) throws Exception {
+		XMLedit XMLeditor = new XMLedit("C:\\Users\\zacfm\\OneDrive\\Documents\\GitHub\\ClientProject\\ClientProjectJava\\testXMLFile.xlsx");
+		XMLeditor.write(0,1,"hello");
+		XMLeditor.close();
 	}
-
+	
 }
 
 
