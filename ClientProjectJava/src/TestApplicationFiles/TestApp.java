bf9a699c4dcdfd2e5f138d4a903185849b6868fc
zacfmhill <zacfmhill@gmail.com>
2020-05-21 16:22:39 -0400

    PDF editing capabilities
    All PDF editing capabilities needed for basic editing and changing of form fields added.

--- ClientProjectJava/src/TestApplicationFiles/TestApp.java
+++ ClientProjectJava/src/TestApplicationFiles/TestApp.java
@@ -1,21 +1,37 @@
 package TestApplicationFiles;
 
 import fileEdit.DOCXedit;
+import fileEdit.PDFedit;
 import fileEdit.XLSXedit;
 import java.io.*;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.pdfbox.pdmodel.interactive.form.PDField;
 import org.apache.poi.xssf.usermodel.*;
 import org.apache.poi.xwpf.usermodel.UnderlinePatterns;
 public class TestApp {
 	
 	public static void main(String[] args) throws Exception {
-		DOCXedit docxTest = new DOCXedit("C:\\Users\\zacfm\\OneDrive\\Desktop\\Chaperone Parking Information.docx");
-		System.out.println(docxTest.extract());
-		docxTest.format(true, true, true, 12, UnderlinePatterns.SINGLE);
+		//DOCXedit docxTest = new DOCXedit("C:\\Users\\zacfm\\OneDrive\\Desktop\\Chaperone Parking Information.docx");
+		//System.out.println(docxTest.extract());
+		//docxTest.format(true, true, true, 12, UnderlinePatterns.SINGLE);
 		//docxTest.writeTable(0,1,0,"test enter");
-		 docxTest.writeTextReplace("test string ", "me in insert");
-		//XLSXedit xlsxTest = new XLSXedit(filelocation);
+		//docxTest.writeTextReplace("test string ", "me in insert");
+		//XLSXedit xlsxTest = new XLSXedit(--------filelocation--------);
 		//xlsxTest.write(0, 3, "test");
 		//xlsxTest.close();
+		//PDFedit pdfTest = new PDFedit("C:\\Users\\zacfm\\OneDrive\\Desktop\\FORMS\\FY 2020 General Field Trip Packet-20200424T154947Z-001\\FY 2020 General Field Trip Packet\\260-1--Fee Waiver Record.pdf");
+		//System.out.println(pdfTest.strip());
+		//pdfTest.setField("2","test input");
+		//List fields = pdfTest.getFields();
+		//Iterator fieldIterator = fields.iterator();
+		//while(fieldIterator.hasNext()) {
+		//	PDField currField = (PDField)fieldIterator.next();
+		//	System.out.println(currField.getFieldType() + " - " + currField.getPartialName()+ " - " +currField.getValueAsString() );
+		//}
+		//pdfTest.save("./testPDF.pdf");
+		
 		
 		
 	}
--- ClientProjectJava/src/fileEdit/PDFedit.java
+++ ClientProjectJava/src/fileEdit/PDFedit.java
@@ -1,5 +1,102 @@
 package fileEdit;
 
-public class PDFedit {
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentCatalog;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.interactive.form.PDAcroForm;
+import org.apache.pdfbox.pdmodel.interactive.form.PDField;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+public class PDFedit {
+	//Instance variables
+	PDDocument document;
+	PDPage page;
+	PDPageContentStream contentStream;
+	PDFont font = PDType1Font.TIMES_ROMAN;
+	PDDocumentCatalog docCatalog;
+	
+	// only for testing, not for final!
+	public PDFedit() throws Exception {
+		document = new PDDocument();
+		page = new PDPage();
+	}
+	//create object from given path
+	public PDFedit(String pathName) throws Exception{
+		File file = new File(pathName);
+		document = PDDocument.load(file);
+		page = new PDPage();
+		docCatalog = document.getDocumentCatalog();
+		
+	}
+	//re assigns the document that you opened from te original file path to a new file path, and assigns doc to this new file path
+	public void duplicateToEditPreserve(String originalFilePath, String newFilePath) throws Exception {
+		File fileOld = new File(originalFilePath);
+		document = PDDocument.load(fileOld);
+		document.save(newFilePath);
+		File fileNew = new File(newFilePath);
+		document = PDDocument.load(fileNew);
+		document.save(newFilePath);
+	}
+	
+	public void addText(String message, int pageNum,int offsetX,int offsetY) throws Exception {
+		page = document.getPage(pageNum);
+		contentStream = new PDPageContentStream(document, page);
+		contentStream.beginText();
+		contentStream.newLineAtOffset(25, 700);
+		contentStream.setFont(font, 12);
+		contentStream.showText(message);
+		contentStream.endText();
+		contentStream.close();
+	}
+	
+	public boolean setField(String fieldName, String fieldValue) throws Exception{
+		document.getNumberOfPages();
+        PDAcroForm acroForm = docCatalog.getAcroForm();
+        PDField field = acroForm.getField(fieldName);
+        if( field != null ) {
+        	System.out.println(field.getActions());
+            field.setValue(fieldValue);
+        }
+        else {
+            return false;
+        }
+		return true;
+	}
+	
+	public List getFields(){
+        PDAcroForm acroForm = docCatalog.getAcroForm();
+        List allFieldsList = acroForm.getFields();
+        return allFieldsList;
+        
+	}
+	
+	
+	//adds object's page to pdf
+	private void addPage(PDPage newPage) {
+		document.addPage(page);
+	}
+	//adds a new blank page to pdf
+	public void newPage() {
+		PDPage newPage = new PDPage();
+		document.addPage(newPage);
+	}
+	//saves the pdf to the given path
+	public void save(String path) throws Exception {
+		document.save(path);
+		document.close();
+	}
+	public String strip() throws Exception {
+		PDFTextStripper pdfStripper = new PDFTextStripper();
+	    String content = pdfStripper.getText(document);
+	    return content;
+	}
 }
